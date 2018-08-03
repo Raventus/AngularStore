2f673818a7c1c8fd8f82dfc8f9a8bf2c8cc822a9
Andrew Koldyrev <koldyrev.andrew@gmail.com>
2018-08-03 22:36:07 +0700

    2

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -8,12 +8,38 @@ import { ProductRepository} from "../model/product.repository";
     templateUrl: "store.component.html"
 })
 export class StoreComponent {
-    constructor (private repository: ProductRepository) {}
-    get products(): Product[] {
-        return this.repository.getProducts();
-    }
+    public selectedCategory = null;
+    public productsPerPage = 4;
+    public selectedPage = 1;
 
+    constructor (private repository: ProductRepository) {}
+
+    get products(): Product[] {
+        let pageIndex = (this.selectedPage - 1)* this.productsPerPage
+        return this.repository.getProducts(this.selectedCategory).slice(pageIndex, pageIndex + this.productsPerPage);
+    }
+    
     get categories(): string[] {
         return this.repository.getCategories();
     }
+
+    changeCategory (newCategory?: string) {
+        this.selectedCategory = newCategory;
+    }
+
+    changePage (newPage: number) {
+        this.selectedPage = newPage;
+    }
+
+    changePageSize (newPageSize: number) {
+        this.productsPerPage = Number (newPageSize);
+        this.changePage (1);
+    }
+
+    get pageCount(): number {
+        return Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage);
+    }
+    /*get pageNumbers(): number[] {
+        return Array(Math.ceil(this.repository.getProducts(this.selectedCategory).length / this.productsPerPage)).fill(0).map((x,i)=> i+1);
+    }*/
 }
